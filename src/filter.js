dde4410e03a759ca8f344908b87a6228af2f6f12
Andy Edwards <jedwards@fastmail.com>
2018-04-01 17:39:20 -0500

    fix(filter): fix predicate type

--- src/filter.js
+++ src/filter.js
@@ -2,7 +2,7 @@
 
 import {$$asyncIterator} from 'iterall'
 
-export default function filter<T>(iterable: AsyncIterable<T>, predicate: (T) => T | Promise<T>): AsyncIterable<T> {
+export default function filter<T>(iterable: AsyncIterable<T>, predicate: (T) => any): AsyncIterable<T> {
   async function * filterAsyncIterator(): AsyncIterator<T> {
     for await (let value of iterable) {
       if (await predicate(value)) yield value
